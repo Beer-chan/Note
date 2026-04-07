05d8d6883cac3448a8f682357cd3198c85d9c494
meshechak-ad <meshechak-ad@MOIT0106>
2026-04-07 17:35:35 +0700

    [eq

---
+++ Note/FirstPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Note;
+
+public partial class FirstPage : ContentPage
+{
+	public FirstPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Login_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
